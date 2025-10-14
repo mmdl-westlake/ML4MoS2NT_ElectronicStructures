679f03df7a7fccf01fb64beaaedb60f71fd8895b
Ju Huang <162401890+JujuHuang@users.noreply.github.com>
2025-10-13 21:56:48 -0400

    Update run_vaspkit.sh

--- scripts/2D_2_1D_map_bands/run_vaspkit.sh
+++ scripts/2D_2_1D_map_bands/run_vaspkit.sh
@@ -2,14 +2,12 @@
 
 module load vaspkit/1.4.0
 
-# Iterate over all directories matching the pattern u_*_N_*
 for dir in u_*_N_*
 do
     if [ -d "$dir" ]; then
         echo "Processing $dir"
         cd "$dir"
 
-        # Run vaspkit and send the commands 21 and 211
         vaspkit <<EOF
 21
 211
